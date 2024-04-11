64004fb88bf3a872d7db4db34921ae9c0ffc3b5e
Luca Novello <luca@lucanovello.com>
2024-04-11 06:48:40 -0400

    updated practice projects - reverse-string

--- practice/reverse-string/main.c
+++ practice/reverse-string/main.c
@@ -1,19 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 void flushKey(void);
 
 int main(void) {
 	char name[31];
-	char nickname[31];
+	char address[51];
+	char reverse[51];
+	
 	printf("Please enter your name: ");
 	scanf("%30[^\n]", &name);
 	flushKey();
-	printf("Please enter your nickname: ");
-	scanf("%30[^\n}", &nickname);
+	
+	printf("Please enter address: ");
+	scanf("%30[^\n]", &address);
 	flushKey();
-	printf("Hello %s\n", name);
-	printf("Your nickname is %s\n", nickname);
+	
+	printf("name: %s\n", name);
+	printf("address: %s\n", address);
+
+	strcpy(reverse, _strrev(name));
+	printf("reversed: %s\n", reverse);
 	return 0;
 }
 
